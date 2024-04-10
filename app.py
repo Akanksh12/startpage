7782a4a02ea91ca0f64938cbe99bb3be5035800d
Akanksh Chitimalla <akanksh.chitimalla@gmail.com>
2024-04-10 19:37:33 +0530

    added storing wallpaper functionality

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from os import mkdir
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -10,7 +12,17 @@ def index():
 @app.route('/settings')
 def settings():
     wallpaper_id = request.args.get("id")
-    if wallpaper_id != '':
+    if wallpaper_id == None:
+        return render_template('settings.html')
+    try:
         mkdir(f'themes/{wallpaper_id}')
-    print(wallpaper_id)
+    except FileExistsError:
+        pass
+    req = requests.get(f'https://wallhaven.cc/api/v1/w/{wallpaper_id}')
+    req = req.json()
+    with open(f'themes/{wallpaper_id}/info.json', 'w+') as file:
+        json.dump(req, file)
+    wallpaper = requests.get(req['data']['path'])
+    with open(f'themes/{wallpaper_id}/wallpaper.png', 'wb') as file:
+        file.write(wallpaper.content)
     return render_template('settings.html')
